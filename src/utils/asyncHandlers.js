040be1ae93da57106b1e7ed6cdcd86f7669beae4
VinayKumarPolepelly <vinaykumarpolepelly30@gmail.com>
2024-05-24 23:21:06 +0530

    connected to mongodb added utils models

---
+++ src/utils/asyncHandlers.js
@@ -0,0 +1,22 @@
+//using promise
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//using async await (try catch)
+// export { asyncHandler };
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
